55c5db5473d6ec6f581b23936d3b67ff9f49d027
tordic <ctordi7545@gmail.com>
2019-04-13 15:08:19 -0500

    big push

--- routes/public.rb
+++ routes/public.rb
@@ -2,4 +2,16 @@ class RateMyDorm < Sinatra::Base
   get '/' do
     erb :index
   end
+
+  get '/reviews.erb' do
+    erb :reviews
+  end
+
+  get '/rate-room.erb' do
+    erb :make_review
+  end
+
+  get '/find-room.erb' do
+    erb :blank
+  end
 end
